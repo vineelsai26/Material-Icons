7c4285217cee0c350d6bad6dbcdb75c0038d98b2
Vineel Sai <mail@vineelsai.com>
2022-06-26 13:08:40 +0530

    access with functions

---
+++ functions/icon/[icon].js
@@ -0,0 +1,25 @@
+export async function onRequest(context) {
+    const {
+        request
+    } = context
+
+    const iconUrl = new URL(request.url)
+    console.log(iconUrl)
+    let iconType = iconUrl.searchParams.get('type')
+    if (iconType && iconType !== 'filled') {
+        iconType = `${iconType.toLowerCase().replaceAll('-', '_')}_`
+    } else {
+        iconType = ''
+    }
+    const icon = `ic_${iconType}${iconUrl.pathname.split('/').pop()}`
+    const newIcon = `${iconUrl.protocol}//${iconUrl.hostname}:${iconUrl.port}/${icon}.html`
+    console.log(newIcon)
+    const res = await fetch(newIcon)
+    const html = await res.text()
+
+    return new Response(html, {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
+}
