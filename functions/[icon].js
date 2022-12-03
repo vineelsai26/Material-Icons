2eb2cd724ab2fb03c15e77f9115ce156ef45db94
Vineel Sai <mail@vineelsai.com>
2022-12-03 22:50:20 +0530

    add comments and dev run script

--- functions/[icon].js
+++ functions/[icon].js
@@ -3,20 +3,30 @@ export async function onRequest(context) {
         request
     } = context
 
+    // Get the icon name from the request
     const iconUrl = new URL(request.url)
-    console.log(iconUrl)
+
+    // Get icon type if exists
     let iconType = iconUrl.searchParams.get('type')
     if (iconType && iconType !== 'filled') {
         iconType = `${iconType.toLowerCase().replaceAll('-', '_')}_`
     } else {
         iconType = ''
     }
+
+    // Create the corresponding icon name
     const icon = `ic_${iconType}${iconUrl.pathname.split('/').pop()}`
+
+    // Create the corresponding icon url
     const newIcon = `${iconUrl.protocol}//${iconUrl.hostname}:${iconUrl.port}/icons/${icon}.html`
-    console.log(newIcon)
+
+    // fetch the icon
     const res = await fetch(newIcon)
+
+    // get the icon content
     const html = await res.text()
 
+    // Return the icon as html
     return new Response(html, {
         headers: {
             "Content-Type": "text/html"
